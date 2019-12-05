4676e383215df057bc876a294eb2f5452d5c86d2
Josh Cardenzana <jcardenzana@irap.omp.eu>
2019-12-05 14:47:31 +0100

    Update timers

--- docs/countdown.js
+++ docs/countdown.js
@@ -2,7 +2,9 @@
 // https://www.w3schools.com/howto/howto_js_countdown.asp
 
 // Set the date we're counting down to
+var startDate     = new Date("Nov 20, 2019 10:00:00 PST").getTime();
 var countDownDate = new Date("Mar 5, 2020 00:00:00 PST").getTime();
+var totalTime     = countDownDate - startDate;
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -11,21 +13,23 @@ var x = setInterval(function() {
   var now = new Date().getTime();
 
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  let distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  let completed = 100*(1-distance/totalTime);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  let msg = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+  document.getElementById("demo").innerHTML = msg;
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "IT'S OVER!";
   }
 }, 1000);
